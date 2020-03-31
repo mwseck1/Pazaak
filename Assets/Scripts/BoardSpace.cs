6cfa855b5bd69c693dfdc3d0708767584f954cca
Michael Seck <michaelseck7@gmail.com>
2020-03-30 21:29:29 -0500

    Added components to allow the cards value to be read by the space it's placed on

---
+++ Assets/Scripts/BoardSpace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSpace : MonoBehaviour
+{
+    int spaceScore;
+    bool hasCard;
+   
+    void OnTriggerEnter2D(Collider2D cardCollider)
+    {
+       spaceScore = cardCollider.gameObject.GetComponent<Card>().value;
+       hasCard = true;
+    }  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceScore = 0;
+        hasCard = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
