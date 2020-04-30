ae27cf2db92aea7fd120037424a4ee1677ff8d25
Michael Seck <michaelseck7@gmail.com>
2020-04-29 22:08:06 -0500

    Fixed issue where script behaved different in build version

--- Assets/Scripts/P1Controller.cs
+++ Assets/Scripts/P1Controller.cs
@@ -9,10 +9,23 @@ public class P1Controller : MonoBehaviour
     public bool canPlayCard;
     public int playerNumber;
 
+    private GameObject[] GetGameSpaces()
+    {
+        GameObject[] spaces = new GameObject[9];
+
+        for(int i = 0; i < 9; i++)
+        {
+            spaces[i] = GameObject.Find("BoardSpaces").GetComponent<Transform>().GetChild(i).gameObject;
+        }
+    
+        return spaces;
+    }
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        player1Spaces = GameObject.FindGameObjectsWithTag("Player1Space");
+        player1Spaces = GetGameSpaces();
         spaceIndex = 0;
         canPlayCard = true;
         playerNumber = 1;
