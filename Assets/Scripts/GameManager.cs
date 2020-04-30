6ca950b871bb845bf00f45624bf5e3e3fbb5a888
Michael Seck <michaelseck7@gmail.com>
2020-04-29 20:13:25 -0500

    Added basic Photon matchmaking

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnPlayerEnteredRoom(Player player)
+    {
+        Debug.Log("Player " + player.NickName + " connected.");
+        
+        if(PhotonNetwork.IsMasterClient)
+        {
+            LoadGameScene(1);
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player player)
+    {
+        Debug.Log("Player " + player.NickName + " disconnected.");
+
+        if(PhotonNetwork.IsMasterClient)
+        {
+            LoadGameScene(0);
+        }
+    }
+
+
+    private void LoadGameScene(int levelNumber)
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(levelNumber);
+        }
+    }
+
+}
--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -2,10 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class Launcher : MonoBehaviour
+public class Launcher : MonoBehaviourPunCallbacks
 {
     string gameVersion = "1";
+    
+    //serialize field allows us to expose private variables in the inspector
+    [SerializeField]
+    private byte maxPlayersPerRoom = 2;
+    [SerializeField]
+    private GameObject controlPanel;
+    [SerializeField]
+    private GameObject connectionText;
+    private bool isConnecting;
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() called");
+        
+        if(isConnecting)
+        {    
+            PhotonNetwork.JoinRandomRoom(); 
+            isConnecting = false;
+        }
+    }   
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        controlPanel.SetActive(true);
+        connectionText.SetActive(false);
+
+        isConnecting = false;
+        
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No room available");
+    
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Room successfully joined.");
+
+        PhotonNetwork.LoadLevel(1);
+    }
 
     void Awake()
     {
@@ -15,19 +60,22 @@ public class Launcher : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Connect();
+        controlPanel.SetActive(true);
+        connectionText.SetActive(false);
     }
 
     public void Connect()
     {
+        connectionText.SetActive(true);
+        controlPanel.SetActive(false);
+
         if(PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.JoinRandomRoom();
-        }
+        }  
         else
         {
             //connects client to server
-            PhotonNetwork.ConnectUsingSettings();
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
             
             //sets version for client
             PhotonNetwork.GameVersion = gameVersion;
